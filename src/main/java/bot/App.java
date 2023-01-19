ac0501ca8085266b2c94e133a3e26a23711194c5
kostiantyn <sinkkostia@gmail.com>
2023-01-20 00:26:09 +0200

    adding bot token, first unbroken version

--- src/main/java/bot/App.java
+++ src/main/java/bot/App.java
@@ -1,7 +1,39 @@
 package bot;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.request.SendMessage;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class App {
     public static void main(String[] args) {
-        System.out.printf("HHH2");
+        String TOKEN = "";
+
+        try {
+            String configFilePath = "src/config.properties";
+            FileInputStream propsInput = new FileInputStream(configFilePath);
+            Properties prop = new Properties();
+            prop.load(propsInput);
+            TOKEN = prop.getProperty("TOKEN");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        TelegramBot bot = new TelegramBot(TOKEN);
+
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(update -> {
+                System.out.println(update);
+                long chatId = update.message().chat().id();
+                String userText = update.message().text();
+                bot.execute(new SendMessage( chatId, userText));
+            });
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+
     }
 }
