50a33fe067b6691e2b0903963ce5a6a8bcc22a55
kostiantyn <sinkkostia@gmail.com>
2023-01-20 00:49:01 +0200

    test commit

--- src/main/java/bot/App.java
+++ src/main/java/bot/App.java
@@ -29,13 +29,6 @@ public class App {
             updates.forEach(update -> {
                 System.out.println(update);
                 long chatId = update.message().chat().id();
-
-
-                System.out.println();
-
-
-
-
                 String userText = update.message().text();
                 bot.execute(new SendMessage(chatId, userText));
             });
