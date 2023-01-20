bd90f3a78d646fc58342bb33ac7992ad14127744
Mykhailo Kovalenko <kovalenko.mykhailo1@lll.kpi.ua>
2023-01-20 17:43:03 +0200

    Adding inline keyboard

--- src/main/java/bot/App.java
+++ src/main/java/bot/App.java
@@ -2,6 +2,8 @@ package bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 
 import java.io.FileInputStream;
@@ -27,13 +29,27 @@ public class App {
 
         bot.setUpdatesListener(updates -> {
             updates.forEach(update -> {
-                System.out.println(update);
-                long chatId = update.message().chat().id();
-                String userText = update.message().text();
-                if (userText != null && update.message() != null) bot.execute(new SendMessage( chatId, userText));
+                System.out.println("\n"+update);
+                if (update.message() != null && update.message().text() != null  && !update.message().from().isBot()) {
+                    long chatId = update.message().chat().id();
+
+                    //String userText = update.message().text();
+                    //bot.execute(new SendMessage(chatId, userText));
+
+                    bot.execute(sendInlineKeyBoardMessage(chatId));
+                } else if (update.callbackQuery() != null){
+                    bot.execute(new SendMessage(update.callbackQuery().message().chat().id(), update.callbackQuery().from().firstName()+" сказал, что он гей"));
+                }
             });
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
 
+    }
+    public static SendMessage sendInlineKeyBoardMessage(long chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton button1 = new InlineKeyboardButton("Я гей");
+        button1.callbackData("pressed");
+        inlineKeyboardMarkup.addRow(button1);
+        return new SendMessage(chatId, "Ты гей?").replyMarkup(inlineKeyboardMarkup);
     }
 }
